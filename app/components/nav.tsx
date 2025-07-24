c798e1b07872d984a40c53384b5e055a3b281545
Nicolas-Eliazer-Jara <nicolas.jara.dev@gmail.com>
2025-07-24 16:34:09 -0300

    primer commit

---
+++ app/components/nav.tsx
@@ -0,0 +1,14 @@
+export function Nav() {
+  return (
+    <div>
+        <div>
+            <button className=" py-2 px-4 bg-[#00b0dd] rounded-[100%] font-black text-white">A</button>
+            <button className=" py-2 px-4 bg-[#ee2234] rounded-[100%] font-black text-white">B</button>
+            <button className=" py-2 px-4 bg-[#0068b5] rounded-[100%] font-black text-white">C</button>
+            <button className=" py-2 px-4 bg-[#08876b] rounded-[100%] font-black text-white">D</button>
+            <button className=" py-2 px-4 bg-[#6b197f] rounded-[100%] font-black text-white">E</button>
+            <button className=" py-2 px-4 bg-[#fed300] rounded-[100%] font-black text-[#56565b]">H</button>
+        </div>
+    </div>
+  )
+}
